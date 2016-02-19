3a0004e4629334e6996b4f6723e80660ee10cc83
Maxwell Krueger <mlk93@pcp093443pcs.mu.edu>
2016-02-18 20:48:56 -0600

    Initial Commit

---
+++ Lab1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lab1
+//
+//  Created by Maxwell Krueger on 2/18/16.
+//  Copyright © 2016 Maxwell Krueger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lab1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab1
+//
+//  Created by Maxwell Krueger on 2/18/16.
+//  Copyright © 2016 Maxwell Krueger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
